8d1ce56f066d740ed1ec5a5e271590d3dd06589f
imanol <imanol.rodriguez@fhios.es>
2020-05-22 12:53:52 +0200

    Fix errors page

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -7,17 +7,17 @@ export default (props) => {
   switch (props.error.type) {
     case 'permission':
       return (
-        <>
+        <div>
           <FontAwesomeIcon icon={faMicrophone}/>
           <h2>This application needs the microphone permission.</h2>
-        </>
+        </div>
       )
     case 'browser':
       return (
-        <>
+        <div>
           <FontAwesomeIcon icon={faChrome}/>
           <h2>Seems like your browser isn't compatible with this technology, try with Chrome web browser.</h2>
-        </>
+        </div>
       )
   }
 }

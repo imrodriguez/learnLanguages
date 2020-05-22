cb0537cf79dfc503057991a168bfdae68c7a7b81
imanol <imanol.rodriguez@fhios.es>
2020-05-22 12:52:12 +0200

    Updates

---
+++ precache-manifest.cf4a35a5d5bf2aaf16396732df914ea7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0528871ab55b70b338bf21db2534560",
+    "url": "/PractiseEnglish/index.html"
+  },
+  {
+    "revision": "73fc80f92e45ed767cd4",
+    "url": "/PractiseEnglish/static/js/2.811b02b4.chunk.js"
+  },
+  {
+    "revision": "bb8fb1c4ca51bfe3d3f76c8caf6f2c92",
+    "url": "/PractiseEnglish/static/js/2.811b02b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "626f634b2bedfd24a539",
+    "url": "/PractiseEnglish/static/js/main.8733e6ca.chunk.js"
+  },
+  {
+    "revision": "7c44292dd00d3cdda442",
+    "url": "/PractiseEnglish/static/js/runtime-main.6609444e.js"
+  }
+]);
